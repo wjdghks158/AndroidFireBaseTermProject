d68b15bf3c081f285331de18095221ba02ce8d3a
wjdghks158 <wjdghks158@naver.com>
2017-12-02 12:12:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/unclej/androidfirebasetermproject/fragment/SecondFragment.java
@@ -0,0 +1,46 @@
+package com.example.unclej.androidfirebasetermproject.fragment;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.unclej.androidfirebasetermproject.R;
+
+
+@SuppressLint("ValidFragment")
+public class SecondFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+    private int mPage;
+
+
+    public SecondFragment(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        //PageFragment fragment1 = new PageFragment(page ,name_Str, location_Str, state, PhoneNum);
+        this.setArguments(args);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+
+    }
+
+    // Inflate the fragment layout we defined above for this fragment
+    // Set the associated text for the title
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = null;
+        if (mPage == 1) {
+            view = inflater.inflate(R.layout.fragment_second, container, false);//fragment_page
+        }
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/unclej/androidfirebasetermproject/fragment/ThirdFragment.java
@@ -0,0 +1,45 @@
+package com.example.unclej.androidfirebasetermproject.fragment;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.unclej.androidfirebasetermproject.R;
+
+@SuppressLint("ValidFragment")
+public class ThirdFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+    private int mPage;
+
+    @SuppressLint("ValidFragment")
+    public ThirdFragment(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        //PageFragment fragment1 = new PageFragment(page ,name_Str, location_Str, state, PhoneNum);
+        this.setArguments(args);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+
+    }
+
+    // Inflate the fragment layout we defined above for this fragment
+    // Set the associated text for the title
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = null;
+        if (mPage == 2) {
+            view = inflater.inflate(R.layout.fragment_third, container, false);//fragment_page
+        }
+
+        return view;
+    }
+}
